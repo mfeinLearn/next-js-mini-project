c3de15325c1a99aa4b8cc9443df98af2df4f894d
Malcome Fein <mfein90@gmail.com>
2026-01-09 17:02:06 -0500

    Done

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -3,11 +3,12 @@ import React from 'react'
 interface User {
     id: number;
     name: string;
+    email: string;
 }
 
 const UsersPage = async () => {
     const res = await fetch(
-        'https://jsonplaceholder.typicode.com/users', 
+        'https://jsonplaceholder.typicode.com/users',
         { cache: 'no-store' });
     // const res = await fetch(
     //     'https://jsonplaceholder.typicode.com/users',
@@ -16,10 +17,21 @@ const UsersPage = async () => {
     return (
         <>
             <h1>Users</h1>
-            <p>{new Date().toLocaleTimeString()}</p>
-            <ul>
-                {users.map(user => <li key={user.id}>{user.name}</li>)}
-            </ul>
+            {/* <p>{new Date().toLocaleTimeString()}</p> */}
+            <table className='table table-bordered'>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Email</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {users.map(user => <tr key={user.id}>
+                        <td>{user.name}</td>
+                        <td>{user.email}</td></tr>)}
+                </tbody>
+
+            </table>
         </>
     )
 }
